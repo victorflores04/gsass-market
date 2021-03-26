a7cc65d49c994d261e61de41f9d502d7bea013cb
victor flores <flores.vicf@gmail.com>
2021-03-26 01:10:02 -0500

    initial proyect

---
+++ src/main/java/com/gsas/market/GsasMarketApplication.java
@@ -0,0 +1,13 @@
+package com.gsas.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsasMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsasMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsas/market/GsasMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsas.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsasMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
